e6c319b5c1065aa028c608744238c9597d1bf0b2
Rasmus Jensen <pr1nce@live.dk>
2020-09-12 23:44:40 +0200

    Major changes

---
+++ Poker/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Poker
+{
+    public class Game
+    {
+        private Player[] players;
+
+        public Game()
+        {
+            Console.WriteLine("How many players?");
+            int playersinput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < (playersinput + 1); i++)
+            {
+                Console.WriteLine("Player name?");
+                string playername = Console.ReadLine();
+                Player player = new Player(playername);
+                players[i] = player;
+            }
+
+            Shuffle shuffle = new Shuffle(players);
+
+            // Ended here .. This shuffle the cards.
+        }
+    }
+}
